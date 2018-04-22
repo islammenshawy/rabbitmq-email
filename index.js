d3bef6d27d39c21e3ed0ccc68fac0d81f5feacce
ielmens <islam_elmenshawy@gap.com>
2018-04-22 12:17:56 -0700

    Added error message queue by phone

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var bodyParser = require('body-parser')
 var http = require('http').Server(app);
 const sgMail = require('@sendgrid/mail');
 
+//Configuration for basic app server
 var amqp_url= process.env.RABBIT_MQ_URL
 var default_queue_name = "ORDER_CONFIRM"
-
 sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
 app.use('/static', express.static('js'))
 app.use(express.json());       // to support JSON-encoded bodies
@@ -15,24 +15,49 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }))
 
+//Quick simulation of dead exchange due to lake of time. Proper way to setup dead exchange is discussed online.
+function sendToErrorQueue(emailData, queue_name){
+  amqp.connect(amqp_url, function(err, conn) {
+    conn.createChannel(function(err, ch) {
+        var q = queue_name + "_ERROR";
+        ch.assertQueue(q, {durable: false});
+        // Note: on Node 6 Buffer.from(msg) should be used
+        ch.sendToQueue(q, new Buffer(JSON.stringify(emailData)));
+        console.log('Sent message data to ERROR Queue: ' + q);
+  });
+  setTimeout(function() { conn.close();}, 500);
+
+});
+}
+
+function isValidMessage(emailData, queue_name){
+  if(emailData.sms && !emailData.phone){
+    console.log("Error sending message as sms without phone.")
+    sendToErrorQueue(emailData, queue_name);
+    return false;
+  }
+  return true;
+}
+
 //Function to send email messages to email.
-function sendEmailMsg(data, queue_name = default_queue_name){
-  var emailData = JSON.parse(data);
-  var email = emailData.email,
+function sendEmailMsg(emailData, queue_name = default_queue_name){
+  if(isValidMessage(emailData, queue_name)){
+      var email = emailData.email,
       name = emailData.name,
       lastname = emailData.lastname,
       phone = emailData.phone,
       sms = emailData.sms,
       body = emailData.body;
-  var subject = "Email from RabbitMQ Queue: " + queue_name;
+      var subject = "Email from RabbitMQ Queue: " + queue_name;
       const msg = {
         to: email,
         from: 'prog.islam@gmail.com',
         subject: subject,
         text: body,
         html: '<strong>This is a test email sent from RabbitMQ.</strong>',
-      };
-  sgMail.send(msg);
+        };
+      sgMail.send(msg);
+  }
 }
 
 function sendToPublisher(data, number_of_times = 1 , queue_name = default_queue_name){
@@ -59,8 +84,8 @@ function invokeListener(queue_name = default_queue_name){
     ch.assertQueue(q, {durable: false});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
-      sendEmailMsg(msg.content.toString(), queue_name);
-      //console.log(" [x] Received %s", msg.content.toString());
+      sendEmailMsg(JSON.parse(msg.content.toString()), queue_name);
+      console.log(" [x] Received %s", msg.content.toString());
     }, {noAck: true});
   });
     setTimeout(function() { conn2.close();}, 500);
